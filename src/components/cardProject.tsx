4fa9ee55a582bb8d68359da5644c085a0b239c5e
Joan Mas <joan.mas23@gmail.com>
2023-11-05 22:43:22 +0100

    correction Image responsive

--- src/components/cardProject.tsx
+++ src/components/cardProject.tsx
@@ -5,20 +5,20 @@ export default function CardProject() {
   const imageStyle = {
     borderRadius: "25%",
     border: "2px solid #fff",
-    width: "50%" ,
-    height: "auto",
+    width: "50%",
+    height: "20%",
   };
 
   return (
     <div className="flex flex-col items-center justify-center text-base font-normal text-center">
       <Image
         src={temp}
-        sizes="20%"
+        sizes="30%"
         alt="project's visual"
         style={imageStyle}
       />
       <div className="p-4">Name of the project | Name of the Entreprise</div>
-      <div className="p-4">Link to GitHub</div>
+      <div className="pt-4">Link to GitHub</div>
     </div>
   );
 }
