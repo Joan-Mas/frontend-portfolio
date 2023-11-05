925981d51ec989370fc3d0538699afe6af4bc80d
Joan Mas <joan.mas23@gmail.com>
2023-11-05 20:10:37 +0100

    responsive about & contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,51 @@
+import Top from "@/components/top";
+import NavBar from "@/components/navbar";
+import Image from "next/image";
+
 export default function About() {
-    return (
-   
-        <div>
-          <p>About</p>
+  const imageStyle = {
+    borderRadius: "50%",
+    border: "2px solid #fff",
+  };
+
+  return (
+    <div className="flex flex-col h-screen text-center bg-[url('/bg.gif')] bg-cover bg-no-repeat text-white">
+      <div className="basis-1/12">
+        <Top />
+      </div>
+      <div className="basis-10/12 font-normal flex flex-col items-center border-2 m-8 rounded-3xl bg-gray-500/20">
+        
+
+        <div className="flex flex-col p-4 text-sm text-left">
+          <div>Language : JavaScript / TypeScript / Java / HTML / CSS</div>
+          <div className="pt-1">Library : React</div>
+          <div className="pt-1">
+            Framework : Next.js / Express.js / Tailwind.css / React Native /
+            Angular
+          </div>
+          <div className="pt-1">Platform : Node.js / Expo</div>
+          <div className="pt-1">Database : MongoDB/NoSQL</div>
+          <div className="pt-1">IDE : Visual Studio Code / Eclipse</div>
         </div>
 
-    )
-  }
-  
+        <div className=" bg-violet-600 flex items-center justify-center border-2 rounded-3xl h-8 w-24 m-4">
+          My CV
+        </div>
+
+        <div className="flex flex-col items-center justify-around p-4">
+          <Image
+            src="/profil.png"
+            width={100}
+            height={100}
+            alt="project's visual"
+            style={imageStyle}
+            className="mt-4"
+          />
+        </div>
+      </div>
+      <div className="basis-1/12">
+        <NavBar />
+      </div>
+    </div>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,23 @@
+import Top from "@/components/top";
+import NavBar from "@/components/navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function Contact() {
-    return (
-
-        <div>
-          <p>Contact</p>
+  return (
+    <div className="flex flex-col h-screen text-center bg-[url('/bg.gif')] bg-cover bg-no-repeat text-white">
+      <div className="basis-1/12">
+        <Top />
+      </div>
+      <div className="basis-10/12 font-normal text-justify flex flex-col items-center justify-center p-16">
+        <div className="pb-4">Need a service, expertise or advice. Get in touch.</div>
+        <div className=" bg-slate-50 flex items-center justify-center border-2 rounded-3xl h-8 w-56 p-1 text-black">
+        <FontAwesomeIcon icon={faEnvelope} className="h-6 pr-1"/>joan.mas23@gmail.com
         </div>
-
-    )
-  }
-  
+      </div>
+      <div className="basis-1/12">
+        <NavBar />
+      </div>
+    </div>
+  );
+}
