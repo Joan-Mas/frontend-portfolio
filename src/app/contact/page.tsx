4cda67b4d29158fe1925a1ad5cc5ca29ffb5d85f
Joan Mas <joan.mas23@gmail.com>
2023-11-06 13:53:36 +0100

    nav bar position fixed -scroll option

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,10 +5,9 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export default function Contact() {  
   return (
     <div className="flex flex-col h-screen text-center bg-[url('/bg.gif')] bg-cover bg-no-repeat text-white">
-      <div style={{ position: "fixed", top: "0", left: "0", width: "100%" }}>
-        <Top />
+      <div className="basis-1/12" style={{ position: 'fixed', top: '0', left: '0', width: '100%' }}>
+        <Top/>
       </div>
-      <div className="basis-1/12"></div>
       <div className="basis-10/12 font-normal text-justify flex flex-col items-center justify-center p-16">
         <div className="pb-4">Need a service, expertise or advice. Get in touch.</div>
         <div className=" bg-slate-50 flex items-center justify-center border-2 rounded-3xl h-8 w-56 p-1 text-black">
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <div className="flex flex-col h-screen text-center bg-[url('/bg.gif')] bg-cover bg-no-repeat text-white">
-      <div className="basis-1/12">
+      <div style={{ position: "fixed", top: "0", left: "0", width: "100%" }}>
         <Top />
       </div>
+      <div className="basis-1/12"></div>
       <div className="basis-10/12 font-normal text-justify flex flex-col items-center justify-around p-16 text-3xl">
         <div><Link href="/">Home</Link></div>
         <div><Link href="/projects">Projects</Link></div>
