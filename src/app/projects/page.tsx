e2753ace2b7f5c8620c0c9f068a0589bf4d5cbb7
Joan Mas <joan.mas23@gmail.com>
2023-11-14 14:19:49 +0100

    wip link to github projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,51 @@
-'use client'
+"use client";
 
 import Top from "@/components/top";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleChevronLeft, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-
-
+import Link from "next/link";
 
 export default function Projects() {
-
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-  }
+  };
 
   const nextSlide = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
-  }
+  };
 
-  
   const slides = [
-    {url : "/rdm.jpg", nameProject: "nameProject", nameEmployer: "Employer"},
-    {url : "/profil.png", nameProject: "PortFolio", nameEmployer: "JoanMas"},
-    {url : "/bg.gif", nameProject: "Space", nameEmployer: "Nasa"},
+    {
+      url: "/tendance.jpg",
+      nameProject: "TendanceApp",
+      nameEmployer: "La Capsule",
+      link: "https://github.com/Joan-Mas/frontend-tendance-app",
+    },
+    {
+      url: "/portfolio.jpg",
+      nameProject: "Site Portfolio",
+      nameEmployer: "Joan Mas",
+      link: "https://github.com/Joan-Mas/frontend-portfolio",
+    },
+    { url: "/rdm.jpg", nameProject: "Work In Progress", nameEmployer: "d-_-b",
+    link: "/", },
   ];
 
   const imageStyle = {
-    borderRadius: "15%",
+    borderRadius: "5%",
     border: "2px solid #fff",
   };
-
-
 
   return (
     <div className="flex flex-col h-screen text-center bg-[url('/bg.gif')] bg-cover bg-no-repeat text-white">
@@ -46,23 +55,33 @@ export default function Projects() {
       <div className="basis-4/12"></div>
       <div className="basis-7/12 items-center justify-center ">
         <div className="flex flex-col items-center justify-center text-base font-normal text-center p-8">
-         <div className="flex flex-row items-center justify-center">
-         <FontAwesomeIcon icon={faCircleChevronLeft} onClick={prevSlide} className="h-6 w-full m-2"/>
-         <Image
-            src={slides[currentIndex].url}
-            sizes="50%"
-            alt="project's visual"
-            width="200"
-            height="200"
-            style={imageStyle}
-            className="duration-500"
-          />
-          <FontAwesomeIcon icon={faCircleChevronRight} onClick={nextSlide} className="h-6 w-full m-2"/>
-         </div>
-          
-          
+          <div className="flex flex-row items-center justify-center">
+            <FontAwesomeIcon
+              icon={faCircleChevronLeft}
+              onClick={prevSlide}
+              className="h-6 w-full m-2"
+            />
+            <Link href={slides[currentIndex].link}>
+              <Image
+                src={slides[currentIndex].url}
+                sizes="50%"
+                alt="project's visual"
+                width="700"
+                height="700"
+                style={imageStyle}
+                className="duration-500"
+              />
+            </Link>
+            <FontAwesomeIcon
+              icon={faCircleChevronRight}
+              onClick={nextSlide}
+              className="h-6 w-full m-2"
+            />
+          </div>
+
           <div className="p-4">
-          {slides[currentIndex].nameProject} | {slides[currentIndex].nameEmployer}
+            {slides[currentIndex].nameProject} |{" "}
+            {slides[currentIndex].nameEmployer}
           </div>
         </div>
       </div>
