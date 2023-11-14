a3be4309c08200e9622acfaaf35f895288313cd3
Joan Mas <joan.mas23@gmail.com>
2023-11-14 15:10:19 +0100

    add h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Top />
       </div>
       <div className="basis-1/12"></div>
-        <div className="basis-10/12 font-normal text-justify flex items-center justify-center p-16">Technical and creative, I design and develop web and mobile applications. I propose innovative and concrete solutions in order to optimize interfaces and solve specific problems.</div>
+        <div className="basis-10/12 font-normal text-justify flex items-center justify-center flex-col p-16"><h1>My name is Joan Mas. I am JavaScript Fullstack Developer.</h1> Technical and creative, I design and develop web and mobile applications. I propose innovative and concrete solutions in order to optimize interfaces and solve specific problems.</div>
         <div className="basis-1/12"></div>
       </div>
     
