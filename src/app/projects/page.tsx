b913003581683475b0da769f0f902539111ed49c
Joan Mas <joan.mas23@gmail.com>
2023-11-14 12:35:30 +0100

    SliderProject 0.1.0

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,34 @@
+'use client'
+
 import Top from "@/components/top";
 import Image from "next/image";
-import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleChevronLeft, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 
 
 export default function Projects() {
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
+  
   const slides = [
-    {url : "/rdm.jpg"},
-    {url : "/profil.png"},
-    {url : "/rdm.jpg"},
+    {url : "/rdm.jpg", nameProject: "nameProject", nameEmployer: "Employer"},
+    {url : "/profil.png", nameProject: "PortFolio", nameEmployer: "JoanMas"},
+    {url : "/bg.gif", nameProject: "Space", nameEmployer: "Nasa"},
   ];
 
   const imageStyle = {
@@ -26,19 +46,23 @@ export default function Projects() {
       <div className="basis-4/12"></div>
       <div className="basis-7/12 items-center justify-center ">
         <div className="flex flex-col items-center justify-center text-base font-normal text-center p-8">
-         
-          <Image
-            src={slides[0].url}
+         <div className="flex flex-row items-center justify-center">
+         <FontAwesomeIcon icon={faCircleChevronLeft} onClick={prevSlide} className="h-6 w-full m-2"/>
+         <Image
+            src={slides[currentIndex].url}
             sizes="50%"
             alt="project's visual"
-            width="300"
-            height="300"
+            width="200"
+            height="200"
             style={imageStyle}
             className="duration-500"
           />
+          <FontAwesomeIcon icon={faCircleChevronRight} onClick={nextSlide} className="h-6 w-full m-2"/>
+         </div>
+          
           
           <div className="p-4">
-            Name of the project | Name of the Entreprise
+          {slides[currentIndex].nameProject} | {slides[currentIndex].nameEmployer}
           </div>
         </div>
       </div>
